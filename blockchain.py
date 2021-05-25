ace6744c36259b9dc5f8a5034122dff57cabfec9
Andrew Lin <andrewlin1368@gmail.com>
2021-05-24 20:27:08 -0400

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -26,7 +26,7 @@ class Blockchain:
         self.new_block(previous_hash='0')
         # testing
         '''
-                self.create_nodes(address='http://127.0.0.1:5000')
+        self.create_nodes(address='http://127.0.0.1:5000')
         self.create_nodes(address='http://127.0.0.1:5001')
         self.create_nodes(address='http://127.0.0.1:5002')
         self.create_nodes(address='http://127.0.0.1:5003')
